1a7fb6c9107f92b7dc3997bdc0dc93f0d8eb550b
Irfan <mamirfan92@gmail.com>
2017-11-05 11:05:26 +0530

    initial commit

---
+++ Omall/Omall/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Omall.MvcApplication" Language="C#" %>
---
+++ Omall/Omall/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Omall.Startup))]
+namespace Omall
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
